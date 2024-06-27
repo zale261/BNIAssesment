8f557b510ae52fdd5234bb6a76e4aaced8ddd81c
Muhammad Hamzalah <39337961+zale261@users.noreply.github.com>
2024-06-27 17:23:09 +0700

    Initial Commit

---
+++ BNIAssesment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BNIAssesment
+//
+//  Created by MacBook on 27/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
